1eaf7e9c86dcf379003ae77b4662b89921f11afd
rafsaf <rafal.safin12@gmail.com>
2021-10-18 01:42:27 +0200

    better add content, added model content to backedn

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,3 +11,11 @@ class User(Base):
     full_name = Column(String(254))
     email = Column(String(254), unique=True, index=True, nullable=False)
     hashed_password = Column(String(128), nullable=False)
+
+
+class Content(Base):
+    __tablename__ = "content"
+
+    id = Column(String(96), primary_key=True, index=True)
+    categories = Column(String(1024), index=True, nullable=False)
+    content = Column(String(50000), nullable=False)
--- frontend/src/pages/AddContent.tsx
+++ frontend/src/pages/AddContent.tsx
@@ -1,7 +1,85 @@
+// @ts-nocheck
 import React from "react";
+import MDEditor from "@uiw/react-md-editor";
+import { Formik, Field, Form, useFormikContext } from "formik";
+import instance from "../api/api";
+
+const AutoSaveToken = () => {
+  // Grab values and submitForm from context
+  const { values, submitForm } = useFormikContext();
+  React.useEffect(() => {
+    // Submit the form imperatively as an effect as soon as form values.token are 6 digits long
+    localStorage.setItem("add-form-title", values.title);
+    localStorage.setItem("add-form-categories", values.categories);
+  }, [values, submitForm]);
+  return null;
+};
+
+function MyEditor() {
+  const [message, setMessage] = React.useState<string>("");
+  const [value, setValue] = React.useState(localStorage.getItem("add-form-md"));
+
+  React.useEffect(() => {
+    localStorage.setItem("add-form-md", value);
+  }, [value]);
+
+  return (
+    <div>
+      <div>
+        <Formik
+          initialValues={{
+            title: localStorage.getItem("add-form-title")
+              ? localStorage.getItem("add-form-title")
+              : "",
+            categories: localStorage.getItem("add-form-categories")
+              ? localStorage.getItem("add-form-categories")
+              : "",
+          }}
+          onReset={async (values) => {
+            values.title = "";
+            values.categories = "";
+            setValue("");
+          }}
+          onSubmit={async (values) => {
+            if (values.title === "" || values.categories === "") {
+              setMessage("Fields cannot be empty");
+              return;
+            }
+            setMessage("");
+            localStorage.removeItem("add-form-title");
+            localStorage.removeItem("add-form-categories");
+            localStorage.removeItem("add-form-md");
+          }}
+        >
+          <Form>
+            <br />
+            {message}
+            <br />
+            <Field id="title" name="title" placeholder="title" />
+            <br />
+            <br />
+            <Field id="categories" name="categories" placeholder="categories" />
+            <br />
+            <br />
+            <MDEditor height={500} value={value} onChange={setValue} />
+            <div style={{ padding: "20px 0 0 0" }} />
+            <button type="submit">Submit</button>
+            <button type="reset">Reset all</button>
+
+            <AutoSaveToken></AutoSaveToken>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+}
 
 const AddContent = () => {
-  return <div>add</div>;
+  return (
+    <div>
+      <MyEditor></MyEditor>
+    </div>
+  );
 };
 
 export default AddContent;
